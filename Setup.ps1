3a177bbf42217d36d220450599607ba0d02af921
iad0N <102708125+iad0N@users.noreply.github.com>
2022-04-17 22:58:54 -0400

    Update Setup.ps1
    Made it so Get-ChildItem outputs a numbered list of the directories. You can then type in the corresponding number to the directory to then copy the contents of it to the desktop. Also instead of using $userpath, I used ~

--- Setup.ps1
+++ Setup.ps1
@@ -1,28 +1,32 @@
-﻿# Reader Source URL
+# Reader Source URL
 $url = "https://admdownload.adobe.com/bin/live/readerdc64_en_xa_crd_install.exe"
 # Reader Destation file
-$userpath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
-$dest = "$userpath\Downloads\reader.exe"
+$dest = "~\Downloads\readerdc64_en_xa_crd_install.exe"
 # Download the Reader file
 Invoke-WebRequest -Uri $url -OutFile $dest
 
 # Ninite 7zip and Chrome Source URL
 $url = "https://ninite.com/7zip-chrome/ninite.exe"
 # Ninite Destation file
-$userpath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
-$dest = "$userpath\Downloads\ninite.exe"
+$dest = "~\Downloads\ninite.exe"
 # Download the Ninite file
 Invoke-WebRequest -Uri $url -OutFile $dest
 
 # Start the Installation of Adobe Reader and Google Chrome and 7zip through Ninite
-start-process "$userpath\Downloads\Reader.exe"
-start-process "$userpath\Downloads\ninite.exe"
+start-process "~\Downloads\readerdc64_en_xa_crd_install.exe" -NoNewWindow
+start-process "~\Downloads\ninite.exe" -NoNewWindow
 
 
-# Get Client Name
-Get-ChildItem -Path .\ -Name -Directory
-$client = Read-Host -Prompt 'Based on the list above, name the client your setting up this computer for'
+# Create a numbered list of directories where the script is located
+Get-ChildItem -Path .\ -Name -Directory | ForEach-Object -Begin {$i=0} -Process {$i++
 
+    "{0} {1}" -f $i,$_} -outvariable list
+    
+    $client = Read-Host 'Which client number?'
+    
+    $target = $list[$client-1].Split()[1]
+    
+    $target
+    
 # Copy client folder contents straight onto the desktop
-Copy-Item ".\$client\*" -Destination "$userpath\Desktop"
-
+Copy-Item ".\$target\*" -Destination "~\Desktop"
